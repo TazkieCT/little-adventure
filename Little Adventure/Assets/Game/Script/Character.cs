a2586d6b97412d128e91efa2a00299122f507b32
Tazkie <100460377+TazkieCT@users.noreply.github.com>
2025-03-22 05:40:29 +0700

    Add Attack Animation & VFX

--- Little Adventure/Assets/Game/Script/Character.cs
+++ Little Adventure/Assets/Game/Script/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:069ca30190b4cfe0ac068e32c72a4bd3daf7ce5288316a5d9bc00cf2bcb1f43d
-size 2452
+oid sha256:7ea65e80ebb905cf762a65009583e370d12ca18aad36416ad9adfe5a8c5db2e6
+size 4318
--- Little Adventure/Assets/Game/Script/PlayerInput.cs
+++ Little Adventure/Assets/Game/Script/PlayerInput.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c40437df16772815629d2347c457ec9be243a08e1c9df640c60ee0565a3f3c6
-size 497
+oid sha256:ff2f80b0fd8a7aff6eb7ccd30526a0a3b245a86ffe04f9522610ff1f15097485
+size 689
