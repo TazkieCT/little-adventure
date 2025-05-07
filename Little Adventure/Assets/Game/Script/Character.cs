d2e15f914eba1d760d99a50841c566232f353d3f
Tazkie <100460377+TazkieCT@users.noreply.github.com>
2025-05-07 19:47:17 +0700

    Enemy 2 now can shoot orb

--- Little Adventure/Assets/Game/Script/Character.cs
+++ Little Adventure/Assets/Game/Script/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c5a4743246292b50db2fa48994fb4c14335cbf4ea5929b628e3fda49e5b97a4
-size 10889
+oid sha256:1a6d906d251976a360c4fc5a93872806c88fa1283d58be65c47d501090b9acd6
+size 11045
---
+++ Little Adventure/Assets/Game/Script/Enemy_02_Shoot.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:97b8d8521aa29cb36c70a64100ef0bc45d41c5c02460a6569d403454e9d16b65
+size 539
